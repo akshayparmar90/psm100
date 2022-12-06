b4a23a3db7769d7469d72ab8fb5906b083bdced2
akshayparmar90 <akshaypparmar@gmail.com>
2022-12-06 22:01:50 +0530

    Solve link issue

--- src/components/Share/Share.jsx
+++ src/components/Share/Share.jsx
@@ -25,7 +25,7 @@ const Share = () => {
         var search_params = url.searchParams;
 
         // new value of "id" is set to "101"
-        search_params.set('n', message);
+        search_params.set('n', message.split(" ").join("").toLocaleLowerCase());
 
         // change the search property of the main url
         url.search = search_params.toString();
